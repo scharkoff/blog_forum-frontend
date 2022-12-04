fcd31f2737ca3834e0fee72845ba12d778916b4f
Sharkov Ivan <sharkov.agent@mail.ru>
2022-12-04 20:34:51 +0300

    Headers for axios
    Access-Control-Allow-Origin for fix CORS error to render.com server

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,10 @@ import axios from "axios";
 // -- Базовый URL для отправки запросов
 const instance = axios.create({
   baseURL: "https://sharkov-blog.onrender.com" || "http://localhost:4444/",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "https://sharkov-blog.onrender.com",
+  },
 });
 
 instance.interceptors.request.use((config) => {
